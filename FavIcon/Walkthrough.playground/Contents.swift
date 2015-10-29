fa9058315aead57a3980f573bd32e3b228ea16bb
Leon Breedt <ljb@bitserf.org>
2015-10-29 18:19:52 +1300

    Improve Playground documentation a bit.

--- FavIcon/Walkthrough.playground/Contents.swift
+++ FavIcon/Walkthrough.playground/Contents.swift
@@ -1,31 +1,36 @@
+//: # FavIcon
 //: Welcome to the introduction for the FavIcon library. I hope you find it useful and easy to use.
 //: I'll walk you through the various usage scenarios.
+//:
+//: *Important:* You'll need Internet access to get the most out of this playground.
 
-import UIKit
+//: First, you need to import `FavIcon` to pull in the library into the current file.
 import FavIcon
-//: First, we need to ensure network requests get a chance to execute.
-import XCPlayground
-XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
-//: Now we're ready. We'll use `downloadPreferred(url:completion:)` first, which tries to download an icon
+//: We'll use `downloadPreferred(url:completion:)` first, which tries to download an icon
 //: matching your desired with and height. Of course, if the website does not have many icons 
-//: to choose from, you may not get the size you desire. But we'll try, at least.
+//: to choose from, you may not get the size you desire.
 //: 
 //: Since downloads happen asynchronously, you need to provide a closure that will get
-//: called when the background task is finished.
-try FavIcons.downloadPreferred(url: "https://microsoft.com", width: 32, height: 32) { result in
-//: The result passed to the closure is a `DownloadResultType`, which is a Swift enum
-//: that will either give you a successful result, with an associated `UIImage`, or
-//: it will give you a failed result, with an associated `ErrorType`.
+//: called when the downloading is finished. This closure will be called from a global queue, so
+//: if you want to do something like update your user interface, you'll need to send it to the main queue
+//: via something like `dispatch_async()`, unless you like weird bugs 😎
+try FavIcons.downloadPreferred(url: "https://apple.com", width: 32, height: 32) { result in
+//: The `result` parameter passed to the closure is a `DownloadResultType`.
+//: This is a Swift enum type that will be `.Success` or `.Failure`.
     switch result {
     case .Success(let image):
+//: In the case of success, the associated value is a `UIImage` on iOS (or a `NSImage` on OS X).
+//: Expand the details for the icon variable in the result view on the right to see the downloaded icon 👍
         let icon = image
         break
     case .Failure(let error):
+//: Hmm, this should not have executed. Do you have Internet connectivity? 🤔
         print("failed - \(error)")
         break
     }
 }
-//: If you want to download all detected icons, call `downloadAll(url:completion:)`.
+
+//: If you want to download all detected icons (maybe you like collecting them), call `downloadAll(url:completion:)`.
 try FavIcons.downloadAll(url: "https://microsoft.com") { results in
     let numberOfIcons = results.count
     for (index, result) in results.enumerate() {
@@ -39,10 +44,26 @@ try FavIcons.downloadAll(url: "https://microsoft.com") { results in
         }
     }
 }
+
 //: If you just want to know which icons are available, you can use the `detect(_:completion:)` method instead.
+//: Note that the width and height of the icons are not always available.
 try FavIcons.detect(url: "https://google.com") { icons in
     for icon in icons {
         let details = "icon: \(icon.url), type \(icon.type), width: \(icon.width), height: \(icon.height)"
     }
 }
+
 //: That's it. Good luck, have fun!
+
+
+
+
+
+
+
+
+
+//: You don't actually need the next two lines when you're using this library, they're just here so that network requests get a chance
+//: to execute when inside a playground.
+import XCPlayground
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
