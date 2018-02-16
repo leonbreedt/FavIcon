96bbda6bcf0eb63c946e98df389b8f655ecfd7b7
Leon Breedt <leon@sector42.io>
2018-02-17 11:51:31 +1300

    Remove unecessary prefix.

--- Sources/FavIcon/ContentTypes.swift
+++ Sources/FavIcon/ContentTypes.swift
@@ -39,28 +39,28 @@ func parseHTTPContentType(_ headerValue: String) -> (mimeType: String, encoding:
 
         return (mimeType: headerComponents[0], encoding: encoding)
     } else {
-        return (mimeType: headerComponents[0], encoding: String.Encoding.utf8)
+        return (mimeType: headerComponents[0], encoding: .utf8)
     }
 }
 
 func parseStringEncoding(_ value: String) -> String.Encoding? {
     switch value.lowercased() {
-    case "iso-8859-1", "latin1": return String.Encoding.isoLatin1
-    case "iso-8859-2", "latin2": return String.Encoding.isoLatin2
-    case "iso-2022-jp": return String.Encoding.iso2022JP
-    case "shift_jis": return String.Encoding.shiftJIS
-    case "us-ascii": return String.Encoding.ascii
-    case "utf-8": return String.Encoding.utf8
-    case "utf-16": return String.Encoding.utf16
-    case "utf-32": return String.Encoding.utf32
-    case "utf-32be": return String.Encoding.utf32BigEndian
-    case "utf-32le": return String.Encoding.utf32LittleEndian
-    case "windows-1250": return String.Encoding.windowsCP1250
-    case "windows-1251": return String.Encoding.windowsCP1251
-    case "windows-1252": return String.Encoding.windowsCP1252
-    case "windows-1253": return String.Encoding.windowsCP1253
-    case "windows-1254": return String.Encoding.windowsCP1254
-    case "x-mac-roman": return String.Encoding.macOSRoman
+    case "iso-8859-1", "latin1": return .isoLatin1
+    case "iso-8859-2", "latin2": return .isoLatin2
+    case "iso-2022-jp": return .iso2022JP
+    case "shift_jis": return .shiftJIS
+    case "us-ascii": return .ascii
+    case "utf-8": return .utf8
+    case "utf-16": return .utf16
+    case "utf-32": return .utf32
+    case "utf-32be": return .utf32BigEndian
+    case "utf-32le": return .utf32LittleEndian
+    case "windows-1250": return .windowsCP1250
+    case "windows-1251": return .windowsCP1251
+    case "windows-1252": return .windowsCP1252
+    case "windows-1253": return .windowsCP1253
+    case "windows-1254": return .windowsCP1254
+    case "x-mac-roman": return .macOSRoman
     default:
         return nil
     }
@@ -71,6 +71,6 @@ extension HTTPURLResponse {
         if let contentType = allHeaderFields["Content-Type"] as? String {
             return parseHTTPContentType(contentType)
         }
-        return (mimeType: "application/octet-stream", encoding: String.Encoding.utf8)
+        return (mimeType: "application/octet-stream", encoding: .utf8)
     }
 }
