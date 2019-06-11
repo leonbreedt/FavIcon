06d06f7364a98b56de79d9aeb3714b53cd8a874d
Leon Breedt <leon@sector42.io>
2019-06-11 23:08:17 +1200

    Update copyright.

--- Sources/FavIcon/Download.swift
+++ Sources/FavIcon/Download.swift
@@ -1,6 +1,6 @@
 //
 // FavIcon
-// Copyright © 2018 Leon Breedt
+// Copyright © 2019 Leon Breedt
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
